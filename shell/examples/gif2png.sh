cb24b9ffa8218fe659ea3b31483bfa62d61c33e9
Andrew Taylor <andrewt@cse.unsw.edu.au>
2011-08-08 11:45:48 +1000

    minor + init seadog

---
+++ shell/examples/gif2png.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# gif2png files...
+# written by geoffw@cse.unsw.edu.au as a COMP2041 example
+# requires PNM graphics conversion tools
+
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 files..." 1>&2
+    exit 1
+fi
+
+if ! type giftopnm 2>/dev/null
+then
+    echo "$0: conversion tool giftopnm not found " 1>&2
+    exit 1
+fi
+
+# missing "in ..." defaults to in "$@"
+for f
+do
+    case "$f" in
+    *.gif)
+        # OK, do nothing
+        ;;
+    *)
+        echo "gif2png: skipping $f, not GIF"
+        continue
+        ;;
+    esac
+
+    dir=`dirname "$f"`
+    base=`basename "$f" .gif`
+    result="$dir/$base.png"
+
+    giftopnm "$f" | pnmtopng > $result && echo "wrote $result"
+done
